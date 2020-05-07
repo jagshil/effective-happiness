1b9673f0e8280bf918b888efc9f85c917871e0e7
jagshil <64651658+jagshil@users.noreply.github.com>
2020-05-07 13:57:08 -0400

    Add files via upload
    Sample Hello Program.

---
+++ helloclass.java
@@ -0,0 +1,11 @@
+//Write a Java Program To Print Hello On Screen.
+package Hello;
+
+public class helloclass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("HELLO this is my first Java Program.");
+	}
+
+}
