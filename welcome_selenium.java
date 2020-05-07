aee810797f77d6725a29692a9ffd8abccf98defd
jagshil <64651658+jagshil@users.noreply.github.com>
2020-05-07 14:50:13 -0400

    Add files via upload
    A Program to Print Welcome to Java Selenium World  + "Your Name"

---
+++ welcome_selenium.java
@@ -0,0 +1,10 @@
+//Write A Program To Take Your Name As Input And Print "Welcome To Java Selenium World + Your Name ".
+package samplejava;
+public class welcome_selenium 
+{
+public static void main(String[] args)
+{
+	String Str="Shilpa";
+	System.out.println("Welcome to Java Selenium World " + Str);
+}
+}
