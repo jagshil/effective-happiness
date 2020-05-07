b60bbfd8cba29e50f2dc073161ff158472ee3d32
jagshil <64651658+jagshil@users.noreply.github.com>
2020-05-07 19:47:52 -0400

    Add files via upload
    An Example to add two numbers and two strings.

---
+++ number_string_addition_example.java
@@ -0,0 +1,17 @@
+package number_string_addition;
+
+public class number_string_addition_example {
+	public static void main (String[] args)
+	{
+		int num1=200;
+		int num2=300;
+		int result= num1+num2; 
+	String S1="Java";
+		String S2= "Selenium";
+		String S3=S1+S2;
+		System.out.println("the addition of two numbers is :"  + result);
+		System.out.println("the addition of two Strings is : " + S3);
+		
+	}
+
+}
