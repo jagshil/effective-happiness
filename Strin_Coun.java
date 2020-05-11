4d94fdaa8a05457fc2dd7984442c470735adc197
jagshil <64651658+jagshil@users.noreply.github.com>
2020-05-11 19:26:03 -0400

    Add files via upload
    Write a Java Program to count the characters of the string;

---
+++ Strin_Coun.java
@@ -0,0 +1,11 @@
+//Write a Java program to count the characters of the string;
+package SampleStrings;
+public class Strin_Coun {
+      public static void main(String[] args)
+      {
+    	  String St= "This is a sample Java program";
+    	  System.out.println(St);
+    	  System.out.println("The length of the above string is: " +St.length());
+    	  
+      }
+}
