75b257ab147042d9fceb71225fa28bc6500fb50e
jagshil <64651658+jagshil@users.noreply.github.com>
2020-05-11 21:58:32 -0400

    Add files via upload
    A program for writing uppercase and lower case letters

---
+++ upperlower.java
@@ -0,0 +1,14 @@
+package SampleStrings;
+
+public class upperlower 
+	{ 
+		public static void main(String[] args)
+		{
+			String str1="Hello World" ;
+			String str2="disney world";
+			System.out.println(str1.toUpperCase());
+			System.out.println(str2.toLowerCase());
+     	}
+		
+	}	
+ 
