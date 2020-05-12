7abf7039ad66c4c897f558eb38c15469bf347d2e
jagshil <64651658+jagshil@users.noreply.github.com>
2020-05-11 21:37:14 -0400

    Add files via upload
    Write a Java program to concate one string to another string

---
+++ Strin_Conca.java
@@ -0,0 +1,16 @@
+//Write a Java program to concate one string to another string;
+package SampleStrings;
+
+public class Strin_Conca
+ {
+ 
+	public static void main(String[] args)
+	
+    {
+	  String str1="Disney";
+	  String str2= str1.concat(" World ");
+	 
+	 System.out.println("the concatenated string is :"   +str2 );
+ }
+ 
+}
