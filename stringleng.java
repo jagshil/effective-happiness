01feef25ca3e07cb9eec5bf0beb9885e6deb3e1d
jagshil <64651658+jagshil@users.noreply.github.com>
2020-05-11 21:45:45 -0400

    Add files via upload
    //Write a program to find out the length of a string

---
+++ stringleng.java
@@ -0,0 +1,13 @@
+//Write a program to find out the length of a string 
+
+package SampleStrings;
+
+public class stringleng
+{
+  public static void main(String[] args)
+  {
+	  String str="Helping Hands Preschool";
+	  
+	  System.out.println(str.length());
+  }
+}
